f12b1c0ceb9d3d8d8688dbf8a5849e6ab1ba59b6
Isabel Solano <misolanob@gmail.com>
2023-02-09 12:36:15 -0600

    Sales second header and time implementation

--- frontend/src/routes/Sales.jsx
+++ frontend/src/routes/Sales.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from './components/header'
+import Header_sales from './components/Header_ventas'
 import Sells_table from './components/Sells_table'
 
 const Sales = ({}) => {
@@ -8,6 +9,7 @@ const Sales = ({}) => {
     return (
         <div>
             <Header title="Ventas"></Header>
+            <Header_sales cajero="prueba"></Header_sales>
             <Sells_table></Sells_table>
         </div>
     )
---
+++ frontend/src/routes/components/Header_ventas.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header_sales = ({ cajero }) => {
+
+  function getCurrentDate(separator = '/') {
+    let newDate = new Date()
+    let date = newDate.getDate()
+    let month = newDate.getMonth() + 1
+    let year = newDate.getFullYear()
+    let hour = newDate.getHours()
+    let minutes = newDate.getMinutes()
+
+    return `${date} / ${month<10?`0${month}`:`${month}`} / ${year} , ${hour}:${minutes}`
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>{cajero}</h2>
+        <h2>{getCurrentDate()}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header_sales
