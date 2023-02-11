a2a155a836f6897dd8bd58730dc51f3711a11e7a
Isabel Solano <misolanob@gmail.com>
2023-02-11 15:20:33 -0600

    basic inventory view

--- frontend/src/routes/Inventory.jsx
+++ frontend/src/routes/Inventory.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Header from './components/header'
-import Inventory_table from './components/Inventory_table'
+import Inventory_table_item from './components/Inventory_table_item'
 
 const Inventory = ({}) => {
+
+  const[productos, setProductos] = useState([])
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      const response = await fetch('http://localhost:4000/productos')
+      const json = await response.json()
+
+      if (response.ok){
+        setProductos(json)
+      }
+    }
+
+    fetchProductos()
+  }, [])
 
   return (
     <div>
       <Header title="Inventory"></Header>
-      <Inventory_table></Inventory_table>
+      <body class="flex items-center justify-center">
+        <div class="container">
+          <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+            <thead class="text-white">
+              <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+              <th class="p-3 text-left">id</th>
+              <th class="p-3 text-left">Producto</th>
+              <th class="p-3 text-left">Precio</th>
+              <th class="p-3 text-left">Existencias</th>
+              <th class="p-3 text-left" width="110px">Actions</th>
+              </tr>
+            </thead>
+            <tbody class="flex-1 sm:flex-none">
+              {/** Elementos de la tabla */}
+              {productos && productos.map((productoC) => (
+                <Inventory_table_item 
+                  id = {productoC.id}
+                  cantidad = {productoC.cantidadDisponible}
+                  producto = {productoC.nombre}
+                  precio = {productoC.precio}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </body>
     </div>
   )
 }
--- frontend/src/routes/components/Inventory_table_item.jsx
+++ frontend/src/routes/components/Inventory_table_item.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Inventory_table_item = ({ Product, Price, Quantity }) => {
+const Inventory_table_item = ({ id, producto, precio, cantidad }) => {
 
   return (
     <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
       <td class="border-grey-light border hover:bg-gray-100 p-3">
-        {Product}
+        {id}
+      </td>
+      <td class="border-grey-light border hover:bg-gray-100 p-3">
+        {producto}
       </td>
       <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">
-        {Price}
+        {precio}
       </td>
       <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
-        {Quantity}
+        {cantidad}
       </td>
       <td class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
-        Delete
+        <button>Delete</button>
       </td>
     </tr>
   )
