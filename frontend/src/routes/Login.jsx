64e091f6c9776102d547bec6a2ce4b37f00a8f48
Isabel Solano <misolanob@gmail.com>
2023-02-12 23:27:13 -0600

    className removal

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -5,23 +5,23 @@ const Login = ({}) => {
 
 
   return (
-    <div className="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
-      <div className="bg-white w-96 shadow-xl rounded p-5">
-        <h1 className="text-3xl font-medium">Welcome</h1>
-        <p className="text-sm">Minimal login page for day to day use</p>
+    <div>
+      <div>
+        <h1>Welcome</h1>
+        <p>Minimal login page for day to day use</p>
         
-        <form className="space-y-5 mt-5">
-          <input type="text" className="w-full h-12 border border-gray-800 rounded px-3" placeholder="Email" />
-          <div className="w-full flex items-center border border-gray-800 rounded px-3">
-            <input type="password" className="w-4/5 h-12" placeholder="Password" />
-            <span className="text-blue-700 hover:bg-blue-400 rounded-md px-3">Show</span>
+        <form>
+          <input type="text" placeholder="Email" />
+          <div >
+            <input type="password" placeholder="Password" />
+            <span>Show</span>
           </div>
   
           <div className="">
-            <a href="#!" className="font-medium text-blue-900 hover:bg-blue-300 rounded-md p-2">Forgot Password ?</a>
+            <a href="#!">Forgot Password ?</a>
           </div>
-          <Link className="login-button-clickable" to="/menu">
-          <button className="text-center w-full bg-blue-900 rounded-md text-white py-3 font-medium">
+          <Link to="/menu">
+          <button>
             
               Login
             
